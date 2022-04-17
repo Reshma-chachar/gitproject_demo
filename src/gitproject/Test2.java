c9da6c6f7066b8dd88db5467fd614d8e6c6ba9ae
Reshma-chachar <reshma.chachar1988@gmail.com>
2022-04-17 19:19:37 +0530

    messege by mayuri

---
+++ src/gitproject/Test2.java
@@ -0,0 +1,18 @@
+package gitproject;
+
+public class Test2 {
+
+	public static void main(String[] args)
+	{
+	String a="yes";
+	if(a=="yes")
+	{
+		System.out.println("eat something");
+	}else
+	{
+		System.out.println("do work");
+	}
+
+	}
+
+}
